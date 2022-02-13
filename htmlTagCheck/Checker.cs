f87e9e8d7291044abe57e30eb044ab2a53d19c52
Khaledul Islam <khaledul.islam@hotmail.com>
2022-02-14 00:42:39 +0600

    Add project files.

---
+++ htmlTagCheck/Checker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace htmlTagCheck
+{
+    internal class Checker
+    {
+        public static string regex = "<([A-Z])>|<(/[A-Z])>";
+        public static string openTag = "<([A-Z])>";
+        public static string TagChecker(string htmlTag)
+        {
+
+            List<char> OpenTags = new();
+            MatchCollection matches = Regex.Matches(htmlTag, regex);
+            foreach (Match item in matches)
+            {
+                if (Regex.Match(item.Value, openTag).Success)
+                {
+                    OpenTags.Add(item.Value[1]);
+                }
+                else
+                {
+                    char closedTag= item.Value[2];
+                    if (!OpenTags.Any())
+                    {
+                        return "Expected # found " + item.Value;
+                    }
+                    char openTag = OpenTags[OpenTags.Count - 1];
+                    if (closedTag.Equals(openTag))
+                    {
+                        OpenTags.RemoveAt(OpenTags.Count - 1);
+                    }
+                    else
+                    {
+                        return "Expected </" + openTag + "> found " + item.Value;
+                    }
+                }
+            }
+            if (OpenTags.Any())
+            {
+                return "Expected </" + OpenTags[^1] + "> found #";
+            }
+            else
+            {
+                return "Correctly tagged paragraph";
+            }
+        }
+    }
+
+}
---
+++ htmlTagCheck/Program.cs
@@ -0,0 +1,9 @@
+﻿using htmlTagCheck;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Input your line below");
+string textRead = Console.ReadLine();
+string result = Checker.TagChecker(textRead);
+Console.WriteLine(result);
+Console.Read();
+
